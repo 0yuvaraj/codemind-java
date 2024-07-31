eaefbeb8d04a62ef26f20839c1fd2c1c7a938c02
0yuvaraj <149353867+0yuvaraj@users.noreply.github.com>
2024-07-31 15:48:53 +0530

    import java.util.*; public class Taps{     public static void main(String[] args){         Scanner read= new Scanner(System.in);         int a,b;         a=read.nextInt();         b=a*a;         int c=4*a;         System.out.printf("%d %d",b,c);     } }

---
+++ Area_and_Perimeter_of_Square.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Taps{
+    public static void main(String[] args){
+        Scanner read= new Scanner(System.in);
+        int a,b;
+        a=read.nextInt();
+        b=a*a;
+        int c=4*a;
+        System.out.printf("%d %d",b,c);
+    }
+}
