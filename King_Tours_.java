700a6a9d386f40abe3fe420fe0b42fa57bb6af3d
0yuvaraj <149353867+0yuvaraj@users.noreply.github.com>
2024-07-31 15:43:03 +0530

    import java.util.*; public class Taps{     public static void main(String[] args){         Scanner read= new Scanner(System.in);         int a,b;         a=read.nextInt();         b=read.nextInt();         int c=a*5+b*7;         System.out.printf("%d",c);     } }

---
+++ King_Tours_.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Taps{
+    public static void main(String[] args){
+        Scanner read= new Scanner(System.in);
+        int a,b;
+        a=read.nextInt();
+        b=read.nextInt();
+        int c=a*5+b*7;
+        System.out.printf("%d",c);
+    }
+}
