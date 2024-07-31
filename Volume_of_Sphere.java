c96bd9d0bcfc3b3c502c0fc391c449d4406e4e10
0yuvaraj <149353867+0yuvaraj@users.noreply.github.com>
2024-07-31 15:52:48 +0530

    import java.util.*; public class Taps{     public static void main(String[] args){         Scanner read= new Scanner(System.in);         int a;         a=read.nextInt();         System.out.printf("%.2f",(float)4/3*3.14*a*a*a);     } }

---
+++ Volume_of_Sphere.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Taps{
+    public static void main(String[] args){
+        Scanner read= new Scanner(System.in);
+        int a;
+        a=read.nextInt();
+        System.out.printf("%.2f",(float)4/3*3.14*a*a*a);
+    }
+}
